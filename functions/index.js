0d1410a70fed82281c09a0a57076d27d7dfe640a
Pegushev Andrey Ivanovich <PROger4ever@gmail.com>
2017-07-15 00:08:55 +0500

    refactoring the function benchmarks a little bit

--- functions/index.js
+++ functions/index.js
@@ -13,56 +13,52 @@ var functionConstructor = new Function("return Math.random();");
 
 var arrowFunction = () => Math.random();
 
-function start() {
-  var res = 1;
+var res = 1;
 
-  console.time("functionDeclaration");
-  for (var i = 0; i < TEST_REPEATS; i++) {
-    res *= functionDeclaration();
-  }
-  console.timeEnd("functionDeclaration");
-
-  console.time("functionExpression");
-  for (var i = 0; i < TEST_REPEATS; i++) {
-    res *= functionExpression();
-  }
-  console.timeEnd("functionExpression");
-
-  console.time("arrowFunction");
-  for (var i = 0; i < TEST_REPEATS; i++) {
-    res *= arrowFunction();
-  }
-  console.timeEnd("arrowFunction");
-
-  console.time("functionConstructor");
-  for (var i = 0; i < TEST_REPEATS; i++) {
-    res *= functionConstructor();
-  }
-  console.timeEnd("functionConstructor");
-
-  console.time("selfInvokingFunction");
-  for (var i = 0; i < TEST_REPEATS; i++) {
-    res *= (function () {
-      return Math.random();
-    })();
-  }
-  console.timeEnd("selfInvokingFunction");
-
-  console.time("eval");
-  for (var i = 0; i < TEST_REPEATS; i++) {
-    res *= (function () {
-      return Math.random();
-    })();
-  }
-  console.timeEnd("eval");
-
-  //NOTE: few last tests work faster O_o ignore it, reorder tests
-  //console.time("functionDeclaration");
-  for (var i = 0; i < TEST_REPEATS; i++) {
-    res *= functionDeclaration();
-  }
-  //console.timeEnd("functionDeclaration");
-  console.log("Prevent the garbage collector and V8 from optimizations on the off-chance", res);
+console.time("functionDeclaration");
+for (var i = 0; i < TEST_REPEATS; i++) {
+  res *= functionDeclaration();
 }
+console.timeEnd("functionDeclaration");
 
-start();
+console.time("functionExpression");
+for (var i = 0; i < TEST_REPEATS; i++) {
+  res *= functionExpression();
+}
+console.timeEnd("functionExpression");
+
+console.time("arrowFunction");
+for (var i = 0; i < TEST_REPEATS; i++) {
+  res *= arrowFunction();
+}
+console.timeEnd("arrowFunction");
+
+console.time("functionConstructor");
+for (var i = 0; i < TEST_REPEATS; i++) {
+  res *= functionConstructor();
+}
+console.timeEnd("functionConstructor");
+
+console.time("selfInvokingFunction");
+for (var i = 0; i < TEST_REPEATS; i++) {
+  res *= (function () {
+    return Math.random();
+  })();
+}
+console.timeEnd("selfInvokingFunction");
+
+console.time("eval");
+for (var i = 0; i < TEST_REPEATS; i++) {
+  res *= (function () {
+    return Math.random();
+  })();
+}
+console.timeEnd("eval");
+
+//NOTE: few last tests work faster O_o ignore it, reorder tests
+//console.time("functionDeclaration");
+for (var i = 0; i < TEST_REPEATS; i++) {
+  res *= functionDeclaration();
+}
+//console.timeEnd("functionDeclaration");
+console.log("Prevent the garbage collector and V8 from optimizations on the off-chance", res);
